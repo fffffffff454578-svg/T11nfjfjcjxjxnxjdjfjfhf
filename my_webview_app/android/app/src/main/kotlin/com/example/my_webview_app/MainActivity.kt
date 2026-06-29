f374c2db405637a0998c3e6db46117baf2e97cb5
fffffffff454578-svg <fffffffff454578@gmail.com>
2026-06-29 08:50:48 +0300

    Delete my_webview_app directory

--- my_webview_app/android/app/src/main/kotlin/com/example/my_webview_app/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.my_webview_app // তোমার নিজের প্যাকেজ নেম যেটা আছে সেটাই
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-    // এখানে বাড়তি কিছু থাকবে না
-}
