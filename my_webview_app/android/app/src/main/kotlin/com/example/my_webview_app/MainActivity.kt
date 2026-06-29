1ceab8c7fad6677f102175ef14de16b75c9332c3
Professor <fffffffff454578@gmail.com>
2026-06-28 21:51:38 -0700

    Uploading hundreds of images and project files

---
+++ my_webview_app/android/app/src/main/kotlin/com/example/my_webview_app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.my_webview_app // তোমার নিজের প্যাকেজ নেম যেটা আছে সেটাই
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+    // এখানে বাড়তি কিছু থাকবে না
+}
---
+++ my_webview_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_inappwebview_macos
+import local_auth_darwin
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
